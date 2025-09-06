0ffb1e5de454bcc928ddf13ec0d8a32293209599
Nabeen-Thapa <nabeen2058@gmail.com>
2025-09-06 10:25:11 +0545

    Add updated portfolio- qualification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import ProjectsSection from '@/components/ProjectsSection'
 import ContactSection from '@/components/ContactSection'
 import './globals.css'
 import QualificationsSection from '@/components/QualificationsSection'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <>
+      <SpeedInsights />
       <HeroSection />
       <SkillsSection />
       <ProjectsSection />
